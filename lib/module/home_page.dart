ee651c93643f92fce885de3242c9fac5195b25d5
AdemDev <33802588+ademgunay@users.noreply.github.com>
2022-04-20 15:14:32 +0700

    refactored rowItem

--- lib/module/home_page.dart
+++ lib/module/home_page.dart
@@ -20,101 +20,63 @@ class HomePage extends StatelessWidget {
             colors: [Colors.red, Colors.blue, Colors.red],
           ),
         ),
-        child: SafeArea(
-          // Our page based on a single column at its root.
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // A row within the column containing an icon and text.
-              Container(
-                // The container will have a margin on left and right of 24
-                margin: const EdgeInsets.symmetric(horizontal: 24),
-                // the decoration allows us to customize our container
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(12),
+        child: Obx(
+          () => SafeArea(
+            // Our page based on a single column at its root.
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // A row within the column containing an icon and text.
+                _rowItem(
+                  Icons.location_city_outlined,
+                  "Your location is ${_controller.address}",
                 ),
-                // We add padding to our row for extra space around it.
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Icon(
-                        Icons.location_city_outlined,
-                        size: 32,
-                      ),
-                      const SizedBox(width: 8),
-                      // This tells the Text to use the maximum space possible.
-                      Expanded(
-                        // We will fetch the user's current city
-                        child: Obx(
-                          () => Text(
-                            "Your location is ${_controller.address}",
-                            style: const TextStyle(fontSize: 18),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
+                const SizedBox(height: 18),
+                _rowItem(
+                  Icons.thermostat_outlined,
+                  "The temperature is ${_controller.temperature}°C",
                 ),
-              ),
-              const SizedBox(height: 18),
-              Container(
-                margin: const EdgeInsets.symmetric(horizontal: 24),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(12),
+                const SizedBox(height: 18),
+                _rowItem(
+                  Icons.info_outline,
+                  "You should ${_controller.infoText.value}",
                 ),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Icon(Icons.thermostat_outlined, size: 32),
-                      const SizedBox(width: 8),
-                      Expanded(
-                        // We will use the city data to fetch the weather
-                        child: Obx(
-                          () => Text(
-                            "The temperature is ${_controller.temperature}°C",
-                            style: const TextStyle(fontSize: 18),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              const SizedBox(height: 18),
-              Container(
-                margin: const EdgeInsets.symmetric(horizontal: 24),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(Icons.info_outline, size: 32),
-                      SizedBox(width: 8),
-                      Expanded(
-                        // We will give advice on what to do
-                        child: Obx(
-                          () => Text(
-                            "You should ${_controller.infoText.value}",
-                            style: TextStyle(fontSize: 18),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              )
-            ],
+              ],
+            ),
           ),
+        ),
+      ),
+    );
+  }
+
+  Widget _rowItem(IconData icon, String text) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 24),
+      // the decoration allows us to customize our container
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      // We add padding to our row for extra space around it.
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              icon,
+              size: 32,
+            ),
+            const SizedBox(width: 8),
+            // This tells the Text to use the maximum space possible.
+            Expanded(
+              // We will fetch the user's current city
+              child: Text(
+                text,
+                style: const TextStyle(fontSize: 18),
+              ),
+            ),
+          ],
         ),
       ),
     );
